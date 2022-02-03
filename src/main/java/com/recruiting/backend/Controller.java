28792a74b8a78d2d831c2b36cfb45c5d16b9f167
asz07 <asz07@mail.aub.edu>
2022-02-04 01:41:06 +0200

    added database configuration to application.properties

--- src/main/java/com/recruiting/backend/Controller.java
+++ src/main/java/com/recruiting/backend/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
   
-  @GetMapping("/home")
+  @GetMapping("/")
   public String home() {
     return "Welcome to Storkrecruit api";
   }
