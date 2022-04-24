0715cf09833203c09c1c176a6ddd92190a2961f2
Ahmad <asz07@mail.aub.edu>
2022-04-24 20:43:49 +0300

    added first name and last name to user entity

--- src/main/java/com/recruiting/backend/model/User.java
+++ src/main/java/com/recruiting/backend/model/User.java
@@ -13,14 +13,20 @@ public class User {
     private String passwordHash;
     @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
 
     public User() { }
 
-    public User(Long id, String username, String passwordHash, String email) {
+    public User(Long id, String username, String passwordHash, String email, String firstName, String lastName) {
         this.id = id;
         this.username = username;
         this.passwordHash = passwordHash;
         this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public Long getId() {
@@ -53,5 +59,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
