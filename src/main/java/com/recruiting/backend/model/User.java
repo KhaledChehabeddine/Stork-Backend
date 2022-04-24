006c397151bf09184d815499dd380b61396d44bb
Ahmad <asz07@mail.aub.edu>
2022-04-24 19:03:04 +0300

    added user entity

---
+++ src/main/java/com/recruiting/backend/model/User.java
@@ -0,0 +1,57 @@
+package com.recruiting.backend.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String passwordHash;
+    @Column(nullable = false)
+    private String email;
+
+    public User() { }
+
+    public User(Long id, String username, String passwordHash, String email) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
